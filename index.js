8a11236d5571dd219961e11a5f15f9ee55fb4048
vagrant <vagrant@precise64.(none)>
2014-11-18 11:34:13 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var _ = require('lodash'),
+	DryCrud,
+	prototypes = {};
+
+prototypes.mongoose = require("./lib/mongoose-prototype");
+prototypes.utils = require("./lib/utils-prototype");
+
+var DryCrud = function(config){
+	
+	//Check for required config
+	if(!config.collection || !config.type){
+
+		console.log("dryCrud error: you must provide a database object and type");
+
+		return false;
+
+	}
+
+	//If the DryCrud object has not yet been instanciated
+	if (!(this instanceof DryCrud)) {
+
+		//Check the type provided
+		if(config.type === 'mongoose'){
+			
+			//Add the appropriate prototype
+			DryCrud.prototype = _.merge(prototypes.utils, prototypes.mongoose);
+
+			console.log(DryCrud.prototype);
+
+		}
+		
+		//Create the DryCrud object
+		return new DryCrud(config);
+
+	}
+
+	//Consume any configuration
+	this.collection = config.collection;
+
+};
+
+
+
+module.exports = DryCrud;

068913451be6477cce4135d09c8c85dd2e298076
paulparton <paulparton@gmail.com>
2014-12-07 09:44:26 +0000

    added adaptor boilerplate

---
+++ adaptor-crudbrella-boilerplate.js
@@ -0,0 +1,27 @@
+//Boilerplace database adaptor for crudbrella
+module.exports =  {
+
+	//Create a new record
+	create: function (options, callback){
+
+	},
+
+	//Read an existing record / records
+	read: function(options, callback){
+
+
+	},
+
+	//Update a record
+	update: function(options, callback){
+
+
+	},
+
+	//Delete a record
+	delete: function(options, callback){
+
+
+	}
+
+};
